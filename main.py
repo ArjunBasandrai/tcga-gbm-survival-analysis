05366e1a84f45c6da1223cbc9735da9ca5c26d89
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2025-02-18 00:14:39 +0530

    feat: updated main file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 os.environ['PROJECT_SEED'] = str(1834579290)
 
@@ -12,30 +13,64 @@ from src.autoencoder.chi2 import chi2_test
 
 from src.config import Conf
 
-clinical_df, encoder = load_clinical_data("data/Clinical.csv")
-mutation_df = load_mutation_data("data/Mutation.csv")
 
-clinical_df = clinical_df[~clinical_df.patient_id.isin(['TCGA.DU.6392', 'TCGA.HT.8564'])]
-mutation_df = mutation_df[~mutation_df.patient_id.isin(['TCGA.DU.6392', 'TCGA.HT.8564'])]
+def train_clinical_autoencoder(device, model, optimizer, loss_fn, scheduler, early_stopping, 
+                               clinical_train_loader, clinical_val_loader, clinical_dataset_all, input_size):
+    train_autoencoder(device, model, optimizer, loss_fn, scheduler, early_stopping, 
+                      clinical_train_loader, clinical_val_loader, plot_path="results/clinical/loss.png")
 
-clinical_train_loader, clinical_val_loader, mutation_train_loader, mutation_val_loader, clinical_dataset_all, mutation_dataset_all = get_dataloaders(clinical_df, mutation_df, batch_size=Conf.batch_size)
+    cv_clinical_autoencoder(device, loss_fn, early_stopping, clinical_dataset_all, input_size)
 
-device, models, optimizers, schedulers, loss_fns, early_stoppings = setup(clinical_df, mutation_df)
+    encoded_clinical_df = encode_clinical_data(model, device, clinical_df)
+    encoded_clinical_df.to_csv("data/EncodedClinical.csv")
 
-# train_autoencoder(device, models[0], optimizers[0], loss_fns[0], schedulers[0], early_stoppings[0], clinical_train_loader, clinical_val_loader, plot_path="results/clinical/loss.png")
-# # cv_clinical_autoencoder(device, loss_fns[0], early_stoppings[0], clinical_dataset_all, clinical_df.shape[1] - 2)
 
-# encoded_clinical_df = encode_clinical_data(models[0], device, clinical_df)
-# print(encoded_clinical_df.head())
+def train_mutation_autoencoder(device, model, optimizer, loss_fn, scheduler, early_stopping, 
+                               mutation_train_loader, mutation_val_loader, mutation_dataset_all, input_size):
+    train_autoencoder(device, model, optimizer, loss_fn, scheduler, early_stopping, 
+                      mutation_train_loader, mutation_val_loader, plot_path="results/mutation/loss.png")
 
-# encoded_clinical_df.to_csv("data/EncodedClinical.csv")
+    # cv_mutation_autoencoder(device, loss_fn, early_stopping, mutation_dataset_all, input_size)
 
-train_autoencoder(device, models[1], optimizers[1], loss_fns[1], schedulers[1], early_stoppings[1], mutation_train_loader, mutation_val_loader, plot_path="results/mutation/loss.png")
-cv_mutation_autoencoder(device, loss_fns[1], early_stoppings[1], mutation_dataset_all, mutation_df.shape[1] - 1)
+    encoded_mutations_data = encode_mutations_data(model, device, mutation_df)
 
-encoded_mutations_data = encode_mutations_data(models[1], device, mutation_df)
-print(encoded_mutations_data.head())
+    encoded_mutations_data.to_csv("data/EncodedMutation.csv")
 
-encoded_mutations_data.to_csv("data/EncodedMutation.csv")
 
-chi2_test(clinical_df, "results/chi2/cluster_vs_survival.png")
+def run_chi2_test(clinical_df):
+    if os.path.exists("data/EncodedMutation.csv") is False:
+        raise ValueError("Please run mutation autoencoder first")
+
+    chi2_test(clinical_df, "results/chi2/cluster_vs_survival.png")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Autoencoder")
+    parser.add_argument("--clinical", action="store_true", help="Run Clinical Autoencoder")
+    parser.add_argument("--mutation", action="store_true", help="Run Mutation Autoencoder")
+    parser.add_argument("--chi2", action="store_true", help="Run Chi2 Test")
+    args = parser.parse_args()
+
+    clinical_df, encoder = load_clinical_data("data/Clinical.csv")
+    mutation_df = load_mutation_data("data/Mutation.csv")
+
+    patients_to_remove = ['TCGA.DU.6392', 'TCGA.HT.8564']
+    clinical_df = clinical_df[~clinical_df.patient_id.isin(patients_to_remove)]
+    mutation_df = mutation_df[~mutation_df.patient_id.isin(patients_to_remove)]
+
+    clinical_train_loader, clinical_val_loader, mutation_train_loader, mutation_val_loader, \
+    clinical_dataset_all, mutation_dataset_all = get_dataloaders(clinical_df, mutation_df, batch_size=Conf.batch_size)
+
+    device, models, optimizers, schedulers, loss_fns, early_stoppings = setup(clinical_df, mutation_df)
+
+    if args.clinical:
+        train_clinical_autoencoder(device, models[0], optimizers[0], loss_fns[0], schedulers[0], early_stoppings[0],
+                                   clinical_train_loader, clinical_val_loader, clinical_dataset_all, 
+                                   clinical_df.shape[1] - 2)
+
+    if args.mutation:
+        train_mutation_autoencoder(device, models[1], optimizers[1], loss_fns[1], schedulers[1], early_stoppings[1],
+                                   mutation_train_loader, mutation_val_loader, mutation_dataset_all, 
+                                   mutation_df.shape[1] - 1)
+
+    if args.chi2:
+        run_chi2_test(clinical_df)
